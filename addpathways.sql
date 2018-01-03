6f15d904416121d26b96ac14c7364d8af360ff1b
tomer <tomer@Ubuntu16.04Dev>
2018-01-03 09:18:35 -0800

    updated getperformancefiles.py and loaddb.py from 101 branch. also upgraded the db columns to match. unit test seems ok

---
+++ addpathways.sql
@@ -0,0 +1,8 @@
+ALTER TABLE clubperf ADD COLUMN (
+	level1s INT DEFAULT 0,
+	level2s INT DEFAULT 0,
+	addlevel2s INT DEFAULT 0,
+	level3s INT DEFAULT 0,
+	level4s INT DEFAULT 0,
+	level5s INT DEFAULT 0
+);
